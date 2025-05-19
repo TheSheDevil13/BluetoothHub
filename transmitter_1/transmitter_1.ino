3c32cf68c1fed17382c0f303e6573e0b075c0163
mymuna-rahman <mymunarahman03@gmail.com>
2025-05-19 22:29:20 +0600

    added fake humidity data

--- transmitter_1/transmitter_1.ino
+++ transmitter_1/transmitter_1.ino
@@ -4,17 +4,21 @@
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define TEMPERATURE_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define HUMIDITY_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a9"
 #define DEVICE_NAME "ESP32_Transmitter1"
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pTemperatureCharacteristic = NULL;
+BLECharacteristic* pHumidityCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 unsigned long previousMillis = 0;
-const long interval = 5000;  
+const long interval = 3000;  
 
 float minTemp = 20.0;
 float maxTemp = 30.0;
+float minHumidity = 40.0;
+float maxHumidity = 60.0;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -45,10 +49,20 @@ void setup() {
                       BLECharacteristic::PROPERTY_NOTIFY
                     );
 
+  pHumidityCharacteristic = pService->createCharacteristic(
+                      HUMIDITY_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
   float initialTemp = (minTemp + maxTemp) / 2;
+  float initialHumidity = (minHumidity + maxHumidity) / 2;
   char tempString[8];
+  char humidityString[8];
   dtostrf(initialTemp, 5, 2, tempString);
+  dtostrf(initialHumidity, 5, 2, humidityString);
   pTemperatureCharacteristic->setValue(tempString);
+  pHumidityCharacteristic->setValue(humidityString);
 
   pService->start();
 
@@ -70,16 +84,23 @@ void loop() {
     previousMillis = currentMillis;
     
     float temperature = random(minTemp * 100, maxTemp * 100) / 100.0;
+    float humidity = random(minHumidity * 100, maxHumidity * 100) / 100.0;
     char tempString[8];
+    char humidityString[8];
     dtostrf(temperature, 5, 2, tempString);
+    dtostrf(humidity, 5, 2, humidityString);
     
     Serial.print("Temperature: ");
     Serial.println(tempString);
+    Serial.print("Humidity: ");
+    Serial.println(humidityString);
     
     pTemperatureCharacteristic->setValue(tempString);
+    pHumidityCharacteristic->setValue(humidityString);
     
     if (deviceConnected) {
       pTemperatureCharacteristic->notify();
+      pHumidityCharacteristic->notify();
     }
   }
   
